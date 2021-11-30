6e03ce9b610cb0ecf9f945b9e639eb78fbf2fa54
Christian Gitter <c.gitter@heurich.de>
2021-11-30 13:16:31 +0100

    add fade

--- js/click.js
+++ js/click.js
@@ -6,12 +6,14 @@ var button2 = document.getElementById('button-plus-2')
 $('.button-plus-1').click(function(){
  div1.style.width = '80%'
  div2.style.width = '20%'
- button1.style.display = 'none'
- button2.style.display = 'block'
+ button1.style.opacity = '0'
+ button1.style.transition = 'opacity 200ms ease-out'
+ button2.style.opacity = '1'
 })
 $('.button-plus-2').click(function(){
     div1.style.width = '20%'
     div2.style.width = '80%'
-    button1.style.display = 'block'
-    button2.style.display = 'none'
+   button2.style.opacity = '0'
+   button2.style.transition = 'opacity 200ms ease-out'
+   button1.style.opacity = '1'
    })
